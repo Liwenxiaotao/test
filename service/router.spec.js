8325b2dcc48dd2c65225e4ca7ffb405b2d15bfc3
unknown <18826497930@163.com>
2019-06-30 17:15:14 +0800

    第一次测试运行

--- service/router.spec.js
+++ service/router.spec.js
@@ -1,16 +1,19 @@
-const axios = require('axios')
+const request = require('supertest')
+const app = require('./app.js')
+
+
 describe("接口", function() {
   it("test接口", function(done) {
-    axios.get('http://localhost:3000/test')
-      .then(function(res) {
-        if(res.data.data === 'hello world') {
+    request(app).get('/test')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err)
+        if (res.body.data === 'hello world') {
           done()
         } else {
-          done(new Error('数据请求错误'))
+          done(new Error('接口数据错误'))
         }
-      })
-      .catch(function(error) {
-        done(error)
       })
   })
 })
